71f24cb2c4cfc9411f1a404ff9516a38dbd54757
MMDShen <80104926+MMDShen@users.noreply.github.com>
2022-06-10 00:19:24 +0430

    Update numbers.php

--- php/numbers.php
+++ php/numbers.php
@@ -5,5 +5,7 @@
     if($a**2==$b**2+$c**2 OR $b**2==$a**2+$c**2 OR $c**2==$b**2+$a**2 )
         echo "YES";
     else
-        echo "NO"; ?>
+        echo "NO";
 # https://quera.org/problemset/280/
+?>
+
