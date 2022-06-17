3b44dd4e2c9247c96d320003c98eb19971d23121
MMDShen <80104926+MMDShen@users.noreply.github.com>
2022-06-17 09:23:40 +0430

    Add files via upload

---
+++ callus-extentions/footer.php
@@ -0,0 +1,52 @@
+<style>
+    footer{
+        position:absolute;
+        display:block;
+        top: 1000px;
+        margin:auto;
+        width: 100%;
+        flex-basis: 0;
+        width:100%;
+        height:600px;
+        background: rgb(26,180,126);
+        background: linear-gradient(187deg, rgba(26,180,126,1) 0%, rgba(92,164,199,1) 59%, rgba(23,96,203,1) 100%); 
+    }
+    .axs{
+        width:15%;
+
+    }
+    footer>div{
+    color:white;
+    font-weight: bold;
+    font-size: 15px;
+    }
+
+</style>
+
+<footer >
+    <div style="width:60%;margin-left:24.6%;">
+        <div style="display:inline;width:50%;height:100%"></div>
+        <div style="display:inline-block;width:50%;height:70%;float:right;margin-top:80px">
+            <div style="width:100%;height:100px;direction:rtl;margin-left:50px;">
+                <img class="axs" src="img/msh.png" alt="maktab">
+            </div>
+            <div style="width:100%;height:100px;direction:rtl">
+                <p>خیابان آزادی، بعد از دانشگاه شریف، نرسیده به میدان آزادی، کوچه آران، پلاک 11،</p>
+                <p>واحد 7 </p>
+                <p>کد پستی: 1458844800</p>
+                <br>
+                <p>ساعات پاسخگویی 9 الی 18 </p>
+                <br>
+                <p>021-91077225</p>
+
+            </div>
+            
+        </div>
+       <hr style="width:100%;height:1px;display:grid;color:white">
+       <p style="text-align:center;direction:rtl">کلیه حقوق ...</p>
+    </div>
+
+
+        
+    
+</footer>
---
+++ callus-extentions/head.php
@@ -0,0 +1,41 @@
+<style>
+    .saghfNav{
+        position:fixed;
+        top:0;
+        left:0;
+        width:100%;
+        background: rgb(26,180,126);
+        background: linear-gradient(187deg, rgba(26,180,126,1) 0%, rgba(92,164,199,1) 59%, rgba(23,96,203,1) 100%); 
+        height:90px;
+        direction:rtl;
+    }
+    img{
+        width:3.5%;
+        padding-right:50px;
+    }
+    .options > p{
+        display:inline-block;
+        padding-right:30px;
+        color:white;
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        font-weight: bolder;
+    }
+    .options{
+        display:flex;
+        margin-top:12px;
+    }
+
+</style>
+<div class="saghfNav">
+   
+    <div class="options"> 
+        <img src="img/msh.png" alt="maktab">
+        <p>مکتب را خوب بشناسیم</p>
+        <p>بوت کمپ ها</p>
+        <p>کاروندان</p>
+        <p>تیم مکتب</p>
+        <p>ارتباط با ما</p>
+        <p>سوالات متداول</p>
+        <p>وبلاگ</p>
+    </div>
+</div>
