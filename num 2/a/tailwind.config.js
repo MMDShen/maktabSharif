f8d808db2004e47bd57b4b06b62560adaabdded9
MMDShen <80104926+MMDShen@users.noreply.github.com>
2022-07-19 17:27:44 +0430

    Add files via upload

---
+++ num 2/a/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    content: ["./css/tailwind.css","./index.html","./num 1/index.html","./num 1/css/tailwind.css"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
